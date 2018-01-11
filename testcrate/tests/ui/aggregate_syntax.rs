cb9359a63c6ca007a3ff7f5e6b8770dda4ccf4ae
Antoni Boucher <bouanto@zoho.com>
2018-01-11 12:07:00 -0500

    Fix aggregate tests

--- testcrate/tests/ui/aggregate_syntax.rs
+++ testcrate/tests/ui/aggregate_syntax.rs
@@ -23,12 +23,10 @@
 
 #![feature(proc_macro)]
 
-extern crate postgres;
 extern crate tql;
 #[macro_use]
 extern crate tql_macros;
 
-use postgres::{Connection, TlsMode};
 use tql::PrimaryKey;
 use tql_macros::sql;
 
@@ -39,13 +37,24 @@ struct Table {
     i32_field: i32,
 }
 
-fn get_connection() -> Connection {
-    Connection::connect("postgres://test:test@localhost/database", TlsMode::None).unwrap()
-}
-
 fn main() {
-    let connection = get_connection();
+    sql!(Table.aggregate(avh(i32_field)));
+    //~^ ERROR unresolved name `avh`
+    //~| HELP did you mean avg?
 
-    sql!(Table.values(test).aggregate(avg(i32_field)));
-    //~^ ERROR attempted access of field `test` on type `Table`, but no field with that name was found
+    sql!(Table.values("test").aggregate(avg(i32_field)));
+    //~^ ERROR Expected identifier
+
+    sql!(Table.aggregate(avg(i32_field, field1)));
+    //~^ ERROR this function takes 1 parameter but 2 parameters were supplied
+
+    sql!(Table.values(i32_field).aggregate(average = avg(i32_field)).filter(avg < 20));
+    //~^ ERROR no aggregate field named `avg` found
+
+    //sql!(Table.values(i32_field).aggregate(average = avg(i32_field)).filter(avrage < 20));
+    // TODO: propose similar names.
+
+    if let Some(aggregate) = sql!(Table.aggregate(average = avg(field2))) {
+        println!("{}", aggregate.averag);
+    }
 }
